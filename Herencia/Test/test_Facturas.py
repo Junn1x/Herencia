37fe8787fd51b8041ca4abdf912502aa8e18c08b
Junn1x <126911671+Junn1x@users.noreply.github.com>
2023-05-14 12:03:44 -0500

    Add files via upload

---
+++ Herencia/Test/test_Facturas.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+path = os.path.abspath("clases/")
+sys.path.append(path)
+
+import pytest
+
+import claseFacturas
+
+def test_datos_vacios():
+   with pytest.raises(TypeError, match='Debes ingresar todos los campos'):
+      datos_cliente = claseFacturas.Factura(productos=None, fechaCompra="",costo="")
+
+def test_costo():
+   with pytest.raises(ValueError, match="El costo solo debe contener numeros"):
+      datos = claseFacturas.Factura(productos=[],fechaCompra="1/06/01",costo="1123sa")
+
+def test_productos():
+   with pytest.raises(TypeError, match="La factura debe tener productos"):
+      datos = claseFacturas.Factura(productos=None,fechaCompra="1/06/01",costo="1123")
